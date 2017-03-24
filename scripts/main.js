3add8f749c966b7228968dc5c76b78e915eff375
tyrant-cdy <1605975462@qq.com>
2017-03-24 13:35:49 +0800

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by cdy on 2017/3/2.
+ */
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'summer!!!';
+document.querySelector('html').onclick = function () {
+    alert('oh,stop poking me!');
+}
